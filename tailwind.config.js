5fb15e5937b8b4b959c4c8d2044e9a9575c8e958
Douglas Santos <88425982+thefoxdeveloper@users.noreply.github.com>
2024-04-28 16:34:39 -0300

    fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
+/** @type {import('tailwindcss').Config} */
 // const colors = require('tailwindcss/colors')
 
 module.exports = {
-  content: ['index.html','./src/**/*.{js,jsx,ts,tsx,vue,html}'],
+  content: ["./**/*.{js,html}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
